2a0718aba01822f7902c356a1f62a040a4946c84
makennamartin97 <makennamartin97@yahoo.com>
2020-09-08 22:24:24 -0400

    get sum of all array elements

---
+++ averybigsum.js
@@ -0,0 +1,6 @@
+//Complete the aVeryBigSum function in the editor below. It must return the 
+//sum of all array elements.
+
+//aVeryBigSum has the following parameter(s):
+
+//int ar[n]: an array of integers .
