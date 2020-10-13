467e2a49077e9f2b837cd8128f8a3dc93e384458
makennamartin97 <makennamartin97@yahoo.com>
2020-10-13 01:40:26 -0400

    hackerrank: python if else

---
+++ pythonifelse.py
@@ -0,0 +1,28 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 == 0:
+        if n in range(2, 5):
+            print("Not Weird")
+        if n in range(6, 20):
+            print("Weird")
+        if n > 20:
+            print("Not Weird")
+    else:
+        print("Weird")
