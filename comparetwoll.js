d1a4524c7ab84740534153f2f718031821814324
makennamartin97 <makennamartin97@yahoo.com>
2020-10-20 16:53:21 -0400

    hackerrank compare 2 linked lists in js

---
+++ comparetwoll.js
@@ -0,0 +1,95 @@
+// hackerrank compare 2 linked lists
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the CompareLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function CompareLists(llist1, llist2) {
+    if(!llist1 && llist2 || llist1 && !llist2){
+        return 0;
+    }
+    let curr1 = llist1;
+    let curr2 = llist2;
+    while(curr1.next && curr2.next){
+        if(curr1.data !== curr2.data){
+            return 0
+        }
+        curr1 = curr1.next
+        curr2 = curr2.next
+    }
+    if(curr1.data === curr2.data && curr1.next === null && curr2.next === null){
+        return 1
+    }
+    return 0
+}
