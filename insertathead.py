911717b989b3922dac0662308b6aea98b77ce97d
makennamartin97 <makennamartin97@yahoo.com>
2020-10-20 19:14:57 -0400

    hackerrank insert node at head in python

---
+++ insertathead.py
@@ -0,0 +1,47 @@
+# insert node at head in python
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtHead function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtHead(llist, data):
+    newnode = SinglyLinkedListNode(data)
+    head = llist
+    if head is None:
+        head = newnode
+        return head
+    else:
+        newnode.next = head
+        head = newnode
+        return head
