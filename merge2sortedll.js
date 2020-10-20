73a950fa7402893c0f7bae69c7f9569be1219f7d
makennamartin97 <makennamartin97@yahoo.com>
2020-10-20 18:06:11 -0400

    hackerrank merge 2 sorted linked lists in js

---
+++ merge2sortedll.js
@@ -0,0 +1,92 @@
+// Given pointers to the heads of two sorted linked lists, merge them into a 
+// single, sorted linked list. Either head pointer may be null meaning that 
+// the corresponding list is empty.
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function mergeLists(head1, head2) {
+    if(!head1 && !head2) return null
+    if(head1 && !head2) return head1
+    if(!head1 && head2) return head2
+    let curr1 = head1
+    let curr2 = head2
+    if(curr1.data <= curr2.data){
+        curr1.next = mergeLists(curr1.next, curr2)
+        return curr1
+    }else if(curr1.data > curr2.data){
+        curr2.next = mergeLists(curr1, curr2.next)
+        return curr2
+    }
+}
