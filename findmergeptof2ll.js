db29fa1c8f61ca19d422dacf3607812ba03def6f
makennamartin97 <makennamartin97@yahoo.com>
2020-10-20 20:58:06 -0400

    hackerrank find merge point of two linked lists in js

---
+++ findmergeptof2ll.js
@@ -0,0 +1,100 @@
+// Given pointers to the head nodes of 2 linked lists that merge together at 
+// some point, find the node where the two lists merge. The merge point is
+// where both lists point to the same node, i.e. they reference the same
+// memory location. It is guaranteed that the two head nodes will be
+// different, and neither will be NULL. If the lists share a common node,
+// return that node's  value.
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+    while (node != null) {
+        ws.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            ws.write(sep);
+        }
+    }
+}
+
+/*
+    Find merge point of two linked lists
+    Note that the head may be 'null' for the empty list.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function findMergeNode(headA, headB) {
+    let a = headA
+    let b = headB
+    while(a.next){
+        let temp = a
+        a = a.next
+        temp.next = null
+    }
+    while(b){
+        b = b.next
+        if(b.next === null){
+            return b.data
+        }
+    }
+    return null
+}
